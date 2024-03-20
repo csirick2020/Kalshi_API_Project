9eaeaa0ec7a95ef105323e8a1f71a9df76776e0f
csirick2020 <144964085+csirick2020@users.noreply.github.com>
2024-03-20 07:13:35 -0500

    Update Kalshi_ROI.py
    Changed code so it could be imported to Kalshi_ROI_GUI

--- Kalshi_ROI_Calc/Kalshi_ROI.py
+++ Kalshi_ROI_Calc/Kalshi_ROI.py
@@ -1,14 +1,15 @@
 # Kalshi ROI Calculator
 
+ROI = ''
+
 def kalshi_ROI(win_ratio, contract_price, stop_loss):
     cost_of_contract = contract_price / 100
     stop_loss_cost = stop_loss / 100
     total_gain = win_ratio * (1.00 - cost_of_contract)
     total_loss = (100 - win_ratio) * (cost_of_contract - stop_loss_cost)
     net_gain = total_gain - total_loss
+    global ROI
     ROI = round((net_gain / contract_price) * 100, 2)
-    print()
-    print(f"ROI (before fees): {ROI} %")
     return ROI
 
 
@@ -17,3 +18,5 @@ if __name__ == "__main__":
     contract_price = int(input("Please enter the (average) contract price in your buying scenario\n(Example: '70' for $0.70)\n"))
     stop_loss = int(input("Please enter your (average) 'stop-loss' price\n(Example: '30' for $0.30)\n"))
     kalshi_ROI(win_ratio, contract_price, stop_loss)
+    print()
+    print(f"ROI (before fees): {ROI} %")
